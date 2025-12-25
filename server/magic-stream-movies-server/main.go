9773cc4f7d12a2b730c65a317cf896f5e5668e8c
Aneesh Rayala <aneeshr0302@gmail.com>
2025-12-25 14:52:00 -0600

    feat: inital setup

---
+++ server/magic-stream-movies-server/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/hello", func(c *gin.Context) {
+		c.String(200, "Hello, World!")
+	})
+
+	router.GET("/movies")
+
+	if err := router.Run(":8080"); err != nil {
+		fmt.Println("Failed to start server: ", err)
+	}
+}
